c991a277d05ce27a0b3371dc3d19cd860a0bb818
yukae <kms980518@gmail.com>
2024-06-16 15:37:44 +0900

    Revert "﻿Revert "feat : Setting Hp""
    This reverts commit d234f13918206610f55f622284df632562f51333.

--- Assets/_Script/Interface/IDamageable.cs
+++ Assets/_Script/Interface/IDamageable.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 public interface IDamageable
 {
     int Health { get; set;}    
 
-    void TakeDamge(int amount);
+    void TakeDamage(int amount);
 }
