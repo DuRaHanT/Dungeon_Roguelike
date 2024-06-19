c0efaa97b7b573f30b53e5cc4f973509012c19d0
yukae <kms980518@gmail.com>
2024-06-19 20:11:59 +0900

    feat : Reset rogic & change unity 2D -> 3D

--- Assets/Srandard Assets/Bomberman/bomberman-assets/README.md
+++
@@ -1,3 +0,0 @@
-# bomberman-assets
-
-All assets for [bomberman](https://github.com/tnicolas42/bomberman) project.
--- Assets/_Script/Interface/IDamageable.cs
+++
@@ -1,8 +0,0 @@
-public interface IDamageable
-{
-    int Health { get; set; }    
-    int Attack { get; set; }
-    int Armor { get; set; }
-
-    void TakeDamage(int amount);
-}
